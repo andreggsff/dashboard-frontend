5b739478f67818afd49de004add26f2a8469a566
Gustavo Gomes <gomesandregustavo@gmail.com>
2025-12-06 18:30:18 -0300

    Fix formatting issues in main.tsx

--- Dashborad-froentend/src/main.tsx
+++ Dashborad-froentend/src/main.tsx
@@ -1,14 +1,12 @@
-// DASHBORAD-FROENTEND/src/main.tsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { AuthProvider } from './contexts/AuthContext.tsx'; // Importa o Provider
+import { AuthProvider } from './contexts/AuthContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 🚨 Envolvemos toda a aplicação com o AuthProvider */}
+
     <AuthProvider> 
       <App />
     </AuthProvider>
