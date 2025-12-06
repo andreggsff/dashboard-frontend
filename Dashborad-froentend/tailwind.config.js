8a35976c941056d814233761cb1ef571f574df18
Gustavo Gomes <gomesandregustavo@gmail.com>
2025-12-06 17:51:50 -0300

    Fix formatting in tailwind.config.js

--- Dashborad-froentend/tailwind.config.js
+++ Dashborad-froentend/tailwind.config.js
@@ -1,14 +1,12 @@
-// dashboard-frontend/tailwind.config.js
-
-/** @type {import('tailwindcss').Config} */
 export default {
-  // O Tailwind irá escanear esses arquivos por classes como "bg-blue-500"
+
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Escaneia todos os arquivos dentro de src/
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+
 }
